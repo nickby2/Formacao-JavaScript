894438979a057441dd5fb177f413aac4fb8e0ec0
Lucas <monteiro9961@gmail.com>
2023-01-23 11:53:38 -0300

    dividir para conquistar

---
+++ aula38_index.js
@@ -0,0 +1,26 @@
+const { edGalho, edFolha } = require('./aula38_array.js');
+
+function juntarListas(lista1, lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+    let atual = 0; // Manejar a posicao atual das listas
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++;
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++;
+        }
+        atual++;
+    }
+
+    return listaFinal;
+}
+
+console.log(juntarListas(edGalho, edFolha));
